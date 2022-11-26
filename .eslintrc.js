3420a700de7efd97785a91d6c495f90a1bf8db6a
yoon-junseo <ysj6844@naver.com>
2022-11-26 23:07:47 +0900

    chore: eslint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/function-component-definition': 0,
+
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
